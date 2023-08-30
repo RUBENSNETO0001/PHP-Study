5b5e60b6907a1fd371a26f21293efc3c6a9af21d
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2023-08-30 14:49:44 -0500

    dsf

---
+++ atividade03/ati.php
@@ -0,0 +1,14 @@
+<?php 
+$carro = 170;
+
+if($carro > 150){
+    echo"a velocidade do carro e maior";
+}
+else if($carro == 150){
+    echo"a velocidade do carro e igual";
+}
+else {
+    echo"a velocidade do carro menor";
+}
+
+?>

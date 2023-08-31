5aab29635c2815529a3e7c4d95eb7a779613ae24
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2023-08-31 10:05:15 -0500

    yttrt

---
+++ atividade04/at.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="style.css">
+<p><body1>
+    
+
+<?php 
+
+$numerosecreto = rand(0, 20);
+$variavel = $numerosecreto;
+$pessoa =10;
+
+
+
+
+if($variavel > $pessoa ){
+ echo"vc errou tente nova mente";
+ 
+}
+else if($variavel < $pessoa){
+echo"vc errou, lixoo";
+
+}
+elseif($variavel === $pessoa){
+echo"vc acertou, oh mizeravi";
+}
+else{
+   echo"cara desiste"; 
+}
+
+?>
+<body>
+
+    <pre></pre>
+<a href="http://localhost/PHP-BASIC/atividade04/at.php" type="button">atualizar</a>
+</body></p>

4426d3060eda5588bc42ac591fc9d564a73e579e
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2023-08-31 14:53:04 -0500

    kty

---
+++ atividade06/at.php
@@ -0,0 +1,16 @@
+<?php 
+$pessoa = 29;
+
+if($pessoa == 1 >  10){
+    echo"vc e criança";
+   }
+elseif($pessoa == 11 > 19){
+    echo"vc e um adolecente";
+   }
+   elseif($pessoa == 20 < 35){
+    echo"vc e um adulto";
+   }
+   elseif($pessoa == 35){
+    echo"vc e um velho";
+   }
+?>

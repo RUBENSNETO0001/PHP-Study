1f055738759bdd90c068480a3f50844037e7546f
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2023-09-06 15:32:23 -0500

    hjud

---
+++ atividade16/c.php
@@ -0,0 +1,8 @@
+<?php 
+
+$array = [];
+
+array_push($array, 1 , 2, 3 , 'test');
+
+var_dump($array)
+?>
---
+++ atividade17/asassaaaaaaaaaaaaaaaaaaaaaaa.php
@@ -0,0 +1,15 @@
+<?php 
+    $contadori = 1;
+           $array = [];
+
+    for($contadori; $contadori <= 10; $contadori++ ){
+        array_push($array, $contadori);
+           echo"<br>";
+       }
+
+
+
+
+var_dump($array)
+
+?>

c7fb64789454affecb2bf32a127e41833df8accd
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2023-09-13 15:00:59 -0500

    er

---
+++ atividade25/asd.php
@@ -0,0 +1,24 @@
+<?php 
+
+$array = [];
+
+array_push($array,' ps5 ', 'xbox one ', 'computador ', ' celular');
+ echo count($array);
+$array [0]= "dds";
+
+foreach($array as$sle ){
+
+echo $sle;
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
